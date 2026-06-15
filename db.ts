b831c82d15764508d489c2eeeaee51cb18b2c5f3
WanderVafla <procenkovladik1@gmail.com>
2026-06-15 14:58:26 +0200

    feat: db return added item

--- db.ts
+++ db.ts
@@ -5,7 +5,7 @@ import type { RawTodo, Todo } from "./types";
 
 const db = new Database("mydb.sqlite", { strict: true });
 export function initBD() {
-const query = db.query(`create table if not exists todos (
+  const query = db.query(`create table if not exists todos (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   title text not null,
   content text,
@@ -16,23 +16,29 @@ const query = db.query(`create table if not exists todos (
   const initBD = query.run();
 }
 
-
-
 export function getTodos(): Todo[] {
   const rawTodos = db.query<RawTodo, []>(`select * from todos`).all();
   return rawTodos.map((todo) => {
     return {
       ...todo,
-      done: Boolean(todo.done)
+      done: Boolean(todo.done),
     };
   });
 }
 
-export function addTodo(elements: Omit<Todo, 'id'>): Omit<Todo, 'id'> {
+export function addTodo(elements: Omit<Todo, "id">): Omit<Todo, "id"> {
   const query = db.query(`
-      INSERT INTO todos (title, content, due_date, done) VALUES (?, ?, ?, ?)
-    `).run(elements.title, elements.content, elements.due_date, elements.done);
-    console.log(getTodos().length);
-    
-    return {...elements}
+      INSERT INTO todos (title, content, due_date, done)
+      VALUES ($title, $content, $due_date, $done)
+      RETURNING *
+    `);
+
+  const insertedTodo = query.get({
+    title: elements.title,
+    content: elements.content ?? null,
+    due_date: elements.due_date ?? null,
+    done: elements.done ? 1 : 0,
+  }) as Todo;
+
+  return { ...insertedTodo, done: Boolean(insertedTodo.done) };
 }

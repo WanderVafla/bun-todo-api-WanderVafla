37444e8016cb93b230eba60564b1900ff6997b1c
WanderVafla <procenkovladik1@gmail.com>
2026-06-17 08:31:46 +0200

    feat(PATCH): added route PATCH for update the task in db

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 import index from "./index.html";
 import { initBD } from "./src/sql/db";
-import { TodoSchema } from "./src/types";
+import { TodoSchema, UpdateTodoSchema, type Todo } from "./src/types";
 import * as v from "valibot";
-import { addTodo, getTodos } from "./src/sql/querys";
+import { addTodo, getTodos, updateData } from "./src/sql/querys";
 
 initBD();
 
@@ -32,6 +32,37 @@ const server = Bun.serve({
         }
       },
     },
+    "/todos/:id": {
+      PATCH: async req => {
+        try {
+          const id = req.params.id
+
+          if (!id) {
+            return Response.json({error: "Missing id in request"}, { status: 404 })
+          }
+          
+          const json = await req.json();
+          const patchData = v.parse(UpdateTodoSchema, json);
+          
+          const item = updateData(Number(req.params.id), { ...patchData })
+
+          if (item && item.error) {
+            return Response.json({error: item.error}, {status: item.status})
+          }
+          
+          return Response.json(req.params.id) 
+        } catch (error) {
+
+          if (v.isValiError(error)) {
+            const errors = v.flatten(error.issues).nested;
+            return Response.json({ errors }, { status: 400 });
+          }
+          
+          console.error("Critical error: ", error);
+          return Response.json({ status: 500 })
+        }
+      }
+    }
   },
 });
 

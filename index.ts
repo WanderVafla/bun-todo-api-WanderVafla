63136c0ea0cb98f8f2c107d134bb20cce339accb
VladysalvGreat <143928231+WanderVafla@users.noreply.github.com>
2026-06-17 14:44:28 +0200

    Wander vafal/delete todo (#14)
    * feat(DELETE): add route for methot DELETE for remove a task from db
    
    * feat(CORS): corsHeader for use API from other app
    
    closes: #7

--- index.ts
+++ index.ts
@@ -7,12 +7,21 @@ import { errors } from "./src/constants";
 
 initBD();
 
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, PATCH, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, prefer",
+};
+
 const server = Bun.serve({
   port: 3000,
   routes: {
     "/": index,
     "/todos": {
-      GET: () => Response.json(getTodos()),
+      OPTIONS: () => {
+        return new Response(null, { status: 204, headers: corsHeaders });
+      },
+      GET: () => Response.json(getTodos(), {headers: corsHeaders}),
       POST: async (req) => {
         try {
           const json = await req.json();
@@ -20,19 +29,31 @@ const server = Bun.serve({
 
           const postTodo = addTodo(post);
 
-          return Response.json({ ...postTodo }, { status: 201 });
+          return Response.json(
+            postTodo,
+            { status: 201, headers: corsHeaders },
+          );
         } catch (error) {
           if (v.isValiError(error)) {
             const errors = v.flatten(error.issues).nested;
-            return Response.json({ errors }, { status: 400 });
+            return Response.json(
+              { error: errors },
+              { status: 400, headers: corsHeaders },
+            );
           }
 
           console.error("Critical error: ", error);
-          return Response.json({ error: error }, { status: 500 });
+          return Response.json(
+            { error: error },
+            { status: 500, headers: corsHeaders },
+          );
         }
       },
     },
     "/todos/:id": {
+      OPTIONS: () => {
+        return new Response(null, { status: 204, headers: corsHeaders });
+      },
       PATCH: async (req) => {
         try {
           const id = req.params.id;
@@ -42,19 +63,41 @@ const server = Bun.serve({
 
           const item = updateData(Number(id), { ...patchData });
 
-          return Response.json(item);
+          return Response.json(item, { status: 201, headers: corsHeaders });
         } catch (error) {
           if (v.isValiError(error)) {
             const errors = v.flatten(error.issues).nested;
-            return Response.json({ errors }, { status: 400 });
+            return Response.json(
+              errors,
+              { status: 400, headers: corsHeaders },
+            );
           }
 
           if (error === errors.SearchError.notFoundId) {
-            return Response.json({ error: error }, { status: 404 });
+            return Response.json(
+              error,
+              { status: 404, headers: corsHeaders },
+            );
           }
 
           console.error(error);
-          return Response.json({ error: `${error}` }, { status: 500 });
+          return Response.json(
+            error,
+            { status: 500, headers: corsHeaders },
+          );
+        }
+      },
+      DELETE: async (req) => {
+        const id = req.params.id;
+
+        try {
+          deleteTodo(Number(id));
+          return new Response(null, { status: 204, headers: corsHeaders });
+        } catch (error) {
+          if (error === errors.QueryError.itemIsNotDeleted) {
+            return new Response(null, { status: 404, headers: corsHeaders });
+          }
+          return new Response(null, { status: 500, headers: corsHeaders });
         }
       },
       DELETE: async (req) => {

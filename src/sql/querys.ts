108a57e08613e0ef17fd580d253a831c029242cd
WanderVafla <procenkovladik1@gmail.com>
2026-06-17 08:29:46 +0200

    feat(SQLite): added function for update a data in db

--- src/sql/querys.ts
+++ src/sql/querys.ts
@@ -1,5 +1,7 @@
+import { object } from "valibot";
 import type { RawTodo, Todo } from "../types";
 import { db } from "./db";
+import { errors } from "../constatns";
 
 export function getTodos(): Todo[] {
   const rawTodos = db.query<RawTodo, []>(`select * from todos`).all();
@@ -26,4 +28,41 @@ export function addTodo(elements: Omit<Todo, "id">): Omit<Todo, "id"> {
   }) as Todo;
 
   return { ...insertedTodo, done: Boolean(insertedTodo.done) };
+}
+
+export function updateData(id: number, data: Partial<Todo>) {
+  const keysData = Object.keys(data);
+  console.log(data);
+
+  const setClause = keysData
+    .map((keyValue) => `${keyValue} = $${keyValue}`)
+    .join(", ");
+  console.log(setClause);
+
+  const variables = Object.entries(data)
+    .map(
+      (keyOrValue) => `${keyOrValue[0]}: "${keyOrValue[1]}"
+                        id: ${id}`,
+    )
+    .join(",\n");
+
+  const check_query = db
+    .query(`
+    SELECT * FROM todos WHERE id = $id
+    `).get({
+        id: id
+      });
+  if (!check_query) {
+    return errors.SearchError.notExistId(id);
+  }
+
+  const query = db.query(`
+      UPDATE todos
+      SET
+        ${setClause}
+      WHERE
+        id = $id
+    `);
+
+  query.run({ ...data, id: id });
 }
